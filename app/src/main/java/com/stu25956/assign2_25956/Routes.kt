09042c1811754da9dd349bf345490affbb2e9650
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2024-03-25 23:26:15 +0000

    Update Routes.kt

--- app/src/main/java/com/stu25956/assign2_25956/Routes.kt
+++ app/src/main/java/com/stu25956/assign2_25956/Routes.kt
@@ -2,5 +2,5 @@ package com.stu25956.assign2_25956
 
 sealed class Routes(val route: String) {
     data object FirstScreen : Routes("Home")
-    data object SecondScreen : Routes("movie_screen")
+    data object SecondScreen : Routes("MovieScreen")
 }
